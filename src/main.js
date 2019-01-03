6a2f3d7fcf7418cebdbc847cbe9f24ab7ccf72bb
JonZhou <23655055@qq.com>
2019-01-03 16:34:03 +0800

    page opened at start

--- src/main.js
+++ src/main.js
@@ -77,18 +77,8 @@ Vue.prototype.$api = services;
 // 用钩子函数beforeEach()对路由进行判断
 
 router.beforeEach((to, from, next) => {
-  if (to.meta.requireAuth) {  // 需要权限,进一步进行判断
-    if (store.state.login.token) {  // 通过vuex state获取当前的token是否存在
-      next();
-    } else {    //如果没有权限,重定向到登录页,进行登录
-      next({
-        path: '/login',
-        // query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-      })
-    }
-  } else { //不需要权限 直接跳转
-    next();
-  }
+  document.body.scrollTop = 0;
+  next();
 })
 
 
