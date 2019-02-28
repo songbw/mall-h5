c7c013b0c12f040b9ffc898577cfed0c4ac0da4d
JonZhou <23655055@qq.com>
2019-02-28 17:38:56 +0800

    update logistics UI

--- src/components/car/pay/logistics.vue
+++ src/components/car/pay/logistics.vue
@@ -4,10 +4,20 @@
       <h1 slot="title">物流信息</h1>
     </v-header>
     <div class="noneInfo">
-      <img :src="no_logistics_bg" />
+      <img :src="no_logistics_bg"/>
       <span>没有查询到物流信息</span>
     </div>
     <div class="logisticsInfo">
+ <!--     <ul>
+        <li v-for="(sku,i)  in k.skus" :key='i' style="list-style: none">
+          <van-card
+            :price="sku.unitPrice"
+            :title="sku.name"
+            :num="sku.num"
+            :thumb="sku.image">
+          </van-card>
+        </li>
+      </ul>-->
       <van-steps direction="vertical" :active="0" active-color="#f44">
         <van-step>
           <h3>【城市】物流状态1</h3>
@@ -38,19 +48,47 @@
         detail: {},
         status: -1,
         no_logistics_bg: require('@/assets/images/truck.png'),
+        logisticsList:[],
       }
     },
 
 
     created() {
-      this.$log("logistics created Enter")
-      this.detail = this.$route.params.detail;
+      let that = this;
+      that.$log("logistics beforeCreate Enter")
+      that.detail = this.$route.params.detail;
+      that.$log(this.detail);
+      that.$api.xapi({
+        method: 'get',
+        url: '/order/logistics',
+        params: {
+          merchantNo: this.detail.merchantNo,
+          orderId:"100102010DFDBF1C25AB@EF6E2A7@AEM1L5D6GBD216246354"//this.detail.tradeNo,
+        }
+      }).then((response) => {
+        that.logisticsList = response.data.data.result;
+        that.$log(that.logisticsList);
+      }).catch(function (error) {
+        that.$log(error)
+      })
     },
 
-    computed: {},
+    computed: {
+
+    },
 
     methods: {
+      isUserEmpty(userInfo) {
+        return (userInfo == undefined || userInfo.length === 0)
+      },
 
+      getSku(skuId) {
+        for( let i = 0; i < this.detail.skus.length ; i++) {
+          if(skuId == this.detail.skus[i].skuId)
+            return this.detail.skus[i];
+        }
+        return null;
+      }
     }
   }
 </script>
