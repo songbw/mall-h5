ba117c2559c7454dc8f37b2b08e90d89d7858680
JonZhou <23655055@qq.com>
2019-03-26 09:46:41 +0800

    add float button for sub index

--- src/components/index/sub/main.vue
+++ src/components/index/sub/main.vue
@@ -1,19 +1,29 @@
 <template lang="html">
-  <div class="index":style="{'background-color': mBackgroundColor}">
-    <v-header>
-      <h1 slot="title">{{title}}</h1>
-    </v-header>
-    <div style="padding: 5px">
-      <li v-for="item in datas" style="list-style: none">
-        <v-swiper v-if="item.type==='0'" :datas="item.data"/>
-        <v-service v-else-if="item.type==='1'" :datas="item.data" :mBackgroundColor="mBackgroundColor"/>
-        <v-sectionSquared v-else-if="item.type==='2'" :datas="item.data" :mBackgroundColor="mBackgroundColor"/>
-        <v-sectionSlide v-else-if="item.type==='3'" :datas="item.data"/>
-        <v-sectionGoods v-else="item.type==='4'" :datas="item.data"/>
-      </li>
+  <section>
+    <div class="index" :style="{'background-color': mBackgroundColor}">
+      <v-header>
+        <h1 slot="title">{{title}}</h1>
+      </v-header>
+      <div style="padding: 5px">
+        <li v-for="item in datas" style="list-style: none">
+          <v-swiper v-if="item.type==='0'" :datas="item.data"/>
+          <v-service v-else-if="item.type==='1'" :datas="item.data" :mBackgroundColor="mBackgroundColor"/>
+          <v-sectionSquared v-else-if="item.type==='2'" :datas="item.data" :mBackgroundColor="mBackgroundColor"/>
+          <v-sectionSlide v-else-if="item.type==='3'" :datas="item.data"/>
+          <v-sectionGoods v-else="item.type==='4'" :datas="item.data"/>
+        </li>
+      </div>
+      <!--    <v-baseline/>-->
     </div>
-<!--    <v-baseline/>-->
-  </div>
+    <div class="float-button-group">
+      <div class="float-button" @click="gotoShoppingCart">
+        <img style="height: 1.5em;width: 1.5em" :src="icon_shopCart">
+      </div>
+      <div class="float-button" @click="gotoOrderList">
+        <img style="height: 1.5em;width: 1.5em" :src="icon_orderList">
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -43,7 +53,9 @@
         datas: {},
         loading: true,
         title: '',
-        mBackgroundColor: '#FFFFFF'
+        mBackgroundColor: '#FFFFFF',
+        icon_orderList: require('@/assets/images/icon_order.png'),
+        icon_shopCart: require('@/assets/images/icon_shopping_cart.png'),
       }
     },
 
@@ -68,7 +80,16 @@
       })
     },
 
-    methods: {}
+    methods: {
+      gotoShoppingCart() {
+        this.$log("gotoShoppingCart Enter")
+        this.$router.push({name: '购物车页'})
+      },
+      gotoOrderList() {
+        this.$log("gotoOrderList Enter")
+        this.$router.push({name: '订单列表页'})
+      },
+    }
   }
 </script>
 
@@ -78,5 +99,25 @@
     width: 100%;
     padding-bottom: 14vw;
     background-color: #F8FCFF;
+  }
+
+  .float-button-group {
+    position: fixed;
+    bottom: 20px;
+    right: 10px;
+
+    .float-button {
+      opacity: 0.8;
+      background: #27282C;
+      margin: 2px;
+      height: 3em;
+      width: 3em;
+      text-align: center;
+      line-height: 3em;
+      border-radius: 50%;
+      -moz-border-radius: 50%;
+      -webkit-border-radius: 50%;
+
+    }
   }
 </style>
