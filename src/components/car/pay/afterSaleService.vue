f2faf17fcc389fd1254c87c701741eb047a9d525
Jon.Zhou <jon-zhou@qq.com>
2019-04-16 12:07:41 +0800

    add afterSaleService Page

--- src/components/car/pay/afterSaleService.vue
+++ src/components/car/pay/afterSaleService.vue
@@ -4,8 +4,23 @@
       <h1 slot="title">售后服务</h1>
     </v-header>
     <div class="serviceBody">
-      <div class="goodsBox"></div>
-      <div class="requireReasonBox"></div>
+      <div class="goodsBox">
+        <van-card
+          :num="goods.num"
+          :price="goods.unitPrice"
+          :title="goods.name"
+          :thumb="goods.image">
+        </van-card>
+        <div slot="footer">
+          <van-cell title="申请数量:" title-class="requireNumberTitle" >
+            <van-stepper v-model="count"/>
+          </van-cell>
+        </div>
+      </div>
+      <div class="requireReasonBox">
+        <span>XXXXXXXX</span>
+
+      </div>
       <div class="discibleQuestion"></div>
       <div class="contactBox"></div>
     </div>
@@ -24,7 +39,8 @@
       return {
         goods: {},
         contact: {},
-        openId: ''
+        openId: '',
+        count:0
       }
     },
 
@@ -33,6 +49,7 @@
       this.goods = this.$route.params.goods;
       this.contact = this.$route.params.contact;
       this.openId = this.$route.params.openId;
+      this.count = this.goods.num;
       this.$log(this.goods)
       this.$log(this.contact)
       this.$log( this.openId)
@@ -48,11 +65,23 @@
   @import "../../../assets/fz.less";
   .afterSaleService{
      .serviceBody{
+       background-color: #f8f8f8;
        .goodsBox{
-
+         .requireNumberTitle{
+           .fz(font-size, 32)
+         }
+         .van-card {
+           background-color: #ffffff;
+           margin-top: 5px;
+           &__price {
+             margin-top: 2em;
+             .fz(font-size, 30);
+           }
+         }
        }
        .requireReasonBox{
-
+         background-color: #ffffff;
+         margin-top: 10px;
        }
        .discibleQuestion{
 
