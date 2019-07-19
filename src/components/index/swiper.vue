d98fc9af187d194d68b9131a466d0f449f1f122f
Jon.Zhou <jon-zhou@qq.com>
2019-07-19 18:23:34 +0800

    add toushi coupon

--- src/components/index/swiper.vue
+++ src/components/index/swiper.vue
@@ -76,7 +76,6 @@
           let userInfo = this.$store.state.appconf.userInfo;
           if (!Util.isUserEmpty(userInfo)) {
             let user = JSON.parse(userInfo);
-          //  targetId +=  "?open_id="+user.userId+"?return_url="+window.location.href+this.$router.currentRoute.fullPath.substring(1);
             targetId +=  "?open_id="+user.userId+"&return_url="+"http://mall.weesharing.com";
             this.$log(targetId)
           }
