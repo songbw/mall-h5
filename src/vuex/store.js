b826ff768a2268e86c6732e5ca519de69cd6932e
Jon.Zhou <jon-zhou@qq.com>
2019-07-22 18:09:46 +0800

    add vuex persisted State

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,16 +1,16 @@
 //store.js
-//import Vue from 'vue'
-//import Vuex from 'vuex'
-
+import createPersistedState from "vuex-persistedstate"
 // 头部分类
 import detail from './modules/detail'
 import category from './modules/category'
 import login from './modules/login'
 import appconf from './modules/appconf'
 
-//Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })],
   modules: {
     appconf,
     detail,
