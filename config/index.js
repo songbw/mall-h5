08f902cbce12b1adeb961496fb677a83c2963c8b
Jon.Zhou <jon-zhou@qq.com>
2019-07-26 11:47:37 +0800

    change  for wuxi config

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     host: '0.0.0.0',
-    port: 1330,
+    port: 1322,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
