9797f81b5d8644876222be03319c2a6ffc661aba
Jon.Zhou <jon-zhou@qq.com>
2019-08-15 14:11:38 +0800

    add gotocart button

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -146,7 +146,7 @@
           //获取关爱通openId
           this.getThirdPartyAccessTokenInfo(auth_code)
         }
-        this.testGAT();
+        //this.testGAT();
       } else {
         this.initJsNativeCb();
         setTimeout(() => {
