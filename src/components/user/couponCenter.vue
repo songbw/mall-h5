84f14a9220b44a6776147ea9737f4540c82ac0a1
Jon.Zhou <jon-zhou@qq.com>
2019-10-07 09:21:49 +0800

    change coupon center UI

--- src/components/user/couponCenter.vue
+++ src/components/user/couponCenter.vue
@@ -477,7 +477,7 @@
 
         .noCoupon {
           width: 100%;
-          margin-top: 100px;
+          padding-top: 100px;
           background-color: #f8f8f8;
           display: flex;
           flex-direction: column;
