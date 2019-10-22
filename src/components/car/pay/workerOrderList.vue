b6e4b712da78966bfab7a1622ffe28966156b952
Jon.Zhou <jon-zhou@qq.com>
2019-10-22 14:25:20 +0800

    change worker list issue

--- src/components/car/pay/workerOrderList.vue
+++ src/components/car/pay/workerOrderList.vue
@@ -127,7 +127,7 @@
               method: 'get',
               baseURL: this.$api.WORKER_ORDER_BASE_URL,
               url: '/customers/work_orders',
-              data: options,
+              params: options,
             }).then((response) => {
               this.result = response.data;
               this.$log(this.result)
