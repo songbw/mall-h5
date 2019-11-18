aec95c0f7be6fb2823740da90d7f7ac652ff4a3b
Jon.Zhou <jon-zhou@qq.com>
2019-11-18 16:08:27 +0800

    add shanghai protocol

--- src/components/user/shanghaiBankProtcol.vue
+++ src/components/user/shanghaiBankProtcol.vue
@@ -4,6 +4,256 @@
       <h1 slot="title">上海银行电商资金管理业务电子协议</h1>
     </v-header>
     <div class="bankCardProtocolBody">
+      <p style="font-size:16pt; line-height:150%; margin:0pt; text-align:center">
+        <span style="font-family:华文细黑; font-size:12pt; font-weight:bold">上海银行电商资金管理业务电子协议</span></p>
+      <p style="margin:0pt; orphans:0; text-align:center; widows:0">
+        <span style="font-family:'Times New Roman'; font-size:16pt; font-weight:bold">&#xa0;</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify">
+        <span style="font-family:仿宋; font-size:12pt">甲方：客户</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify">
+        <span style="font-family:仿宋; font-size:12pt">乙方：上海银行股份有限公司</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify">
+        <span style="font-family:仿宋; font-size:12pt">丙方</span>
+        <span style="font-family:仿宋; font-size:12pt">：</span>
+        <span style="font-family:仿宋; font-size:12pt">深圳凤巢信息科技有限公司</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; orphans:0; text-align:justify; widows:0">
+        <span style="font-family:仿宋; font-size:12pt">&#xa0;</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">甲方自愿通过</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">申请使用乙方提供的电商资金管理业务，为明确</span>
+        <span style="font-family:仿宋; font-size:12pt">各</span>
+        <span style="font-family:仿宋; font-size:12pt">方的权利和义务，维护</span>
+        <span style="font-family:仿宋; font-size:12pt">各</span>
+        <span style="font-family:仿宋; font-size:12pt">方的合法权益，特签订本服务协议。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">甲方在显示本协议的</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">页面或APP上点击确认或以乙方约定的其他方式选择接受本协议，即表示甲方与乙方已达成协议并同意接受本协议的全部约定内容以及与本协议有关的各项规则规定</span>
+        <span style="font-family:仿宋; font-size:12pt">。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">您接受</span>
+        <span style="font-family:仿宋; font-size:12pt">本协议，即视为</span>
+        <span style="font-family:仿宋; font-size:12pt">授权</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方</span>
+        <span style="font-family:仿宋; font-size:12pt">及其</span>
+        <span style="font-family:仿宋; font-size:12pt">合作方</span>
+        <span style="font-family:仿宋; font-size:12pt">根据本协议</span>
+        <span style="font-family:仿宋; font-size:12pt">代表您</span>
+        <span style="font-family:仿宋; font-size:12pt">向</span>
+        <span style="font-family:仿宋; font-size:12pt">乙方发送</span>
+        <span style="font-family:仿宋; font-size:12pt">电商资金管理业务</span>
+        <span style="font-family:仿宋; font-size:12pt">项下的各指令</span>
+        <span style="font-family:仿宋; font-size:12pt">，</span>
+        <span style="font-family:仿宋; font-size:12pt">包括但不限于</span>
+        <span style="font-family:仿宋; font-size:12pt">资金登记</span>
+        <span style="font-family:仿宋; font-size:12pt">、资金提款</span>
+        <span style="font-family:仿宋; font-size:12pt">、</span>
+        <span style="font-family:仿宋; font-size:12pt">查询等</span>
+        <span style="font-family:仿宋; font-size:12pt">交易</span>
+        <span style="font-family:仿宋; font-size:12pt">指令。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">在接受本协议之前，请您仔细阅读本协议的全部内容。如果您不同意本协议的任意内容，或者无法准确理解</span>
+        <span style="font-family:仿宋; font-size:12pt">乙方</span>
+        <span style="font-family:仿宋; font-size:12pt">对条款的解释，请不要进行后续操作。否则，视为您已准确理解并全部接受本协议内容。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">&#xa0;</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第一条  甲方同意</span>
+        <span style="font-family:仿宋; font-size:12pt">通过</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">使用乙方提供的电商资金管理服务（以下简称“本服务”）。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">二</span>
+        <span style="font-family:仿宋; font-size:12pt">条  甲方应首先在</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">注册交易账户，并提交相关身份证明资料，经丙方平台备案，通过</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">核实身份后</span>
+        <span style="font-family:仿宋; font-size:12pt">向</span>
+        <span style="font-family:仿宋; font-size:12pt">乙方</span>
+        <span style="font-family:仿宋; font-size:12pt">发起申请</span>
+        <span style="font-family:仿宋; font-size:12pt">。</span>
+        <span style="font-family:仿宋; font-size:12pt">乙方</span>
+        <span style="font-family:仿宋; font-size:12pt">根据</span>
+        <span style="font-family:仿宋; font-size:12pt">甲方申请，</span>
+        <span style="font-family:仿宋; font-size:12pt">为甲方</span>
+        <span style="font-family:仿宋; font-size:12pt">完成</span>
+        <span style="font-family:仿宋; font-size:12pt">商户</span>
+        <span style="font-family:仿宋; font-size:12pt">记账簿注册及验证后</span>
+        <span style="font-family:仿宋; font-size:12pt">，甲方可使用</span>
+        <span style="font-family:仿宋; font-size:12pt">本</span>
+        <span style="font-family:仿宋; font-size:12pt">服务。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">商户记账簿是指乙方为甲方</span>
+        <span style="font-family:仿宋; font-size:12pt">提供电商资金管理</span>
+        <span style="font-family:仿宋; font-size:12pt">服务</span>
+        <span style="font-family:仿宋; font-size:12pt">而设立</span>
+        <span style="font-family:仿宋; font-size:12pt">的</span>
+        <span style="font-family:仿宋; font-size:12pt">商户登记编号</span>
+        <span style="font-family:仿宋; font-size:12pt">，用于记录甲方在</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">交易中的资金变动</span>
+        <span style="font-family:仿宋; font-size:12pt">余额</span>
+        <span style="font-family:仿宋; font-size:12pt">及</span>
+        <span style="font-family:仿宋; font-size:12pt">明细。该</span>
+        <span style="font-family:仿宋; font-size:12pt">记账簿</span>
+        <span style="font-family:仿宋; font-size:12pt">不构成乙方对甲方的负债义务。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">三</span>
+        <span style="font-family:仿宋; font-size:12pt">条</span>
+        <span style="width:20pt; text-indent:0pt; display:inline-block"></span>
+        <span style="font-family:仿宋; font-size:12pt">甲方通过丙方平台在线向乙方申请开通本服务。甲方需提供真实有效的身份信息，具体内容以乙方要求为准。甲方须对所提供上述资料的真实性、有效性、完整性负责。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">四</span>
+        <span
+          style="font-family:仿宋; font-size:12pt">条  甲方保证其资金来源合法。甲方明确知晓丙方平台的交易风险，清楚认识并愿意承担相关风险，同意遵守银行结算规则和相关法律法规。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">五</span>
+        <span style="font-family:仿宋; font-size:12pt">条  乙方有权利委托第三方验证甲方身份信息，并识别、留存第三方传递给乙方的甲方身份信息。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">六</span>
+        <span style="font-family:仿宋; font-size:12pt">条  甲方承诺向乙方提供的所有资料真实、有效、完整、准确、合法，未由他人代办或冒充他人申请。如甲方提供的资料不符合要求或存在代办、冒充他人申请等情形，乙方有权拒绝甲方的申请直至终止协议。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">七</span>
+        <span style="font-family:仿宋; font-size:12pt">条  甲方开通本</span>
+        <span style="font-family:仿宋; font-size:12pt">服务</span>
+        <span style="font-family:仿宋; font-size:12pt">后，乙方可为甲方提供以下功能：</span></p>
+      <p
+        style="font-size:12pt; line-height:150%; margin:0pt; orphans:0; text-align:justify; text-indent:21pt; widows:0">
+        <span style="font-family:仿宋; font-size:12pt">（</span>
+        <span style="font-family:仿宋; font-size:12pt">一</span>
+        <span style="font-family:仿宋; font-size:12pt">）</span>
+        <span style="font-family:仿宋; font-size:12pt">支付结算类服务，是指甲方接受乙方所属</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">的委托</span>
+        <span style="font-family:仿宋; font-size:12pt">向乙方提供</span>
+        <span style="font-family:仿宋; font-size:12pt">商户</span>
+        <span style="font-family:仿宋; font-size:12pt">记账簿</span>
+        <span style="font-family:仿宋; font-size:12pt">开立、</span>
+        <span style="font-family:仿宋; font-size:12pt">同名结算</span>
+        <span style="font-family:仿宋; font-size:12pt">账户绑定及鉴权验证、担保交易、提现、交易资金清分、清算和对账等一体化资金管理服务。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:21pt">
+        <span style="font-family:仿宋; font-size:12pt">（二） 提现服务，是指将资金从商户记账簿转入</span>
+        <span style="font-family:仿宋; font-size:12pt">商户的</span>
+        <span style="font-family:仿宋; font-size:12pt">同名银行结算账户</span>
+        <span style="font-family:仿宋; font-size:12pt">。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第八条  甲方</span>
+        <span style="font-family:仿宋; font-size:12pt">绑定</span>
+        <span style="font-family:仿宋; font-size:12pt">的同名</span>
+        <span
+          style="font-family:仿宋; font-size:12pt">银行结算账户卡、折或相关凭证及印鉴遗失或被盗后，须及时办理挂失手续，因甲方未及时办理挂失手续而引起的责任或造成的损失，由甲方承担。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第九条  本服务终止之前，甲方不得将本服务对应的同名银行结算账户销户。乙方不承担因甲方将服务对应的同名银行结算账户销户导致资金无法划转的法律责任。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">十</span>
+        <span style="font-family:仿宋; font-size:12pt">条  甲方承诺：通过甲方签约的丙方平台向乙方发送的任何指令均系甲方本人作出，即乙方有理由相信对于丙方平台发来由甲方作出的任何指令（违反甲方与</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">签订的相关交易规则的除外），甲方均已同意。乙方有权依据该指令对甲方开立在乙方的</span>
+        <span style="font-family:仿宋; font-size:12pt">商户记账簿</span>
+        <span style="font-family:仿宋; font-size:12pt">进行处理。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十一条  甲方应保证本协议中提供的同名银行结算账户为非贷记类银行结算账户。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十二条  甲方如因下列原因不能正常使用本服务时，乙方享有免责权利。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（一） 甲方</span>
+        <span style="font-family:仿宋; font-size:12pt">提供</span>
+        <span style="font-family:仿宋; font-size:12pt">的</span>
+        <span style="font-family:仿宋; font-size:12pt">同名银行结算账户发生挂失或被依法冻结等情形；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（</span>
+        <span style="font-family:仿宋; font-size:12pt">二</span>
+        <span style="font-family:仿宋; font-size:12pt">） 同名银行结算账户已销户或解除绑定关系；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（</span>
+        <span style="font-family:仿宋; font-size:12pt">三</span>
+        <span style="font-family:仿宋; font-size:12pt">） 由于非乙方因素导致的电子指令信息无法辨认、接收及处理的；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（四） 因甲方签约</span>
+        <span style="font-family:仿宋; font-size:12pt">的</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台与乙方解除业务合作而造成损失的</span>
+        <span style="font-family:仿宋; font-size:12pt">。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十三条  乙方已与丙方平台签订了合作协议，共同为甲方提供服务，甲方可从丙方平台获取该协议文本，并仔细阅读该协议责任条款，准确理解有关各方责任。甲方与其签约丙方平台的任何纠纷、争议由其与丙方平台自行解决，与乙方无关</span>
+        <span style="font-family:仿宋; font-size:12pt">。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十四条 </span>
+        <span style="font-family:仿宋; font-size:12pt">各</span>
+        <span style="font-family:仿宋; font-size:12pt">方在履行本协议的过程中如有任何</span>
+        <span style="font-family:仿宋; font-size:12pt">违</span>
+        <span style="font-family:仿宋; font-size:12pt">反本协议条款的行为，守约方有权要求违约方承担违约责任及赔偿因其违约行为而给守约方造成的损失。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十五条 因战争、自然灾害等不可抗力以及非当事人过错造成的网络故障、系统运行故障等情形，致使一方或双方无法履行本协议约定的义务的，不承担违约责任。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第十六条 因履行本协议所引起的及与本协议有关的纠纷，双方应友好协商解决；协商不成时，应向乙方所在地法院提起诉讼。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:仿宋; font-size:12pt">十七</span>
+        <span style="font-family:仿宋; font-size:12pt">条  若出现以下情况，本协议自动失效：</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（一）经甲方申请，丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">终止</span>
+        <span style="font-family:仿宋; font-size:12pt">本服务；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（二）甲方签约丙方平台与乙方解除合作关系；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（三）监管部门要求</span>
+        <span style="font-family:仿宋; font-size:12pt">终止</span>
+        <span style="font-family:仿宋; font-size:12pt">本服务；</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（四）法律另有规定的。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">第</span>
+        <span style="font-family:宋体; font-size:12pt">十八</span>
+        <span style="font-family:仿宋; font-size:12pt">条 </span>
+        <span style="font-family:仿宋; font-size:12pt">各</span>
+        <span style="font-family:仿宋; font-size:12pt">方</span>
+        <span style="font-family:仿宋; font-size:12pt">确认并声明：</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（一）甲方保证其资金来源合法，其向乙方提供的所有证件、资料均合法、真实、准确、完整和有效。甲方资料发生变化时，甲方必须及时通知乙方并办理相关变更手续。</span>
+      </p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（二）甲方同意遵守有关法律法规及</span>
+        <span style="font-family:仿宋; font-size:12pt">丙方平台</span>
+        <span style="font-family:仿宋; font-size:12pt">交易规则。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（</span>
+        <span style="font-family:仿宋; font-size:12pt">三</span>
+        <span style="font-family:仿宋; font-size:12pt">）</span>
+        <span style="font-family:仿宋; font-size:12pt">丙</span>
+        <span style="font-family:仿宋; font-size:12pt">方保证向</span>
+        <span style="font-family:仿宋; font-size:12pt">乙</span>
+        <span style="font-family:仿宋; font-size:12pt">方发送数据及指令的准确性、及时性、真实性</span>
+        <span style="font-family:仿宋; font-size:12pt">，并确保交易的背景真实。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">（</span>
+        <span style="font-family:仿宋; font-size:12pt">四</span>
+        <span style="font-family:仿宋; font-size:12pt">）乙方已提请甲方注意对本协议</span>
+        <span style="font-family:仿宋; font-size:12pt">中</span>
+        <span style="font-family:仿宋; font-size:12pt">各条款作全面、准确的理解，并应甲方要求就本协议的相关条款进行了充分的解释说明。</span>
+        <span style="font-family:仿宋; font-size:12pt">各方</span>
+        <span style="font-family:仿宋; font-size:12pt">对本协议含义认识一致。</span>
+        <span style="font-family:仿宋; font-size:12pt">各方</span>
+        <span style="font-family:仿宋; font-size:12pt">理解并同意今后所进行的一切</span>
+        <span style="font-family:仿宋; font-size:12pt">服务</span>
+        <span style="font-family:仿宋; font-size:12pt">均受本协议之约束。</span></p>
+      <p style="font-size:12pt; line-height:150%; margin:0pt; text-align:justify; text-indent:28pt">
+        <span style="font-family:仿宋; font-size:12pt">&#xa0;</span></p>
     </div>
   </section>
 </template>
@@ -33,9 +283,7 @@
       this.showHeader = this.$api.HAS_HEADER;
     },
 
-    methods: {
-
-    }
+    methods: {}
   }
 </script>
 
@@ -48,10 +296,9 @@
     width: 100%;
     height: 100%;
     top: 0px;
-    background-color: #f8f8f8;
 
     .bankCardProtocolBody {
-      background-color: #f8f8f8;
+      margin: 0px 10px;
     }
   }
 </style>
