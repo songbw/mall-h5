d5b6127c62166d5a58f1a5a54d737c1411ce90d5
Jon.Zhou <jon-zhou@qq.com>
2019-11-28 13:49:12 +0800

    change UI

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,18 +5,18 @@
     </v-header>
     <div class="loginBody">
       <div class="loginBind">
-          <div class="bindTitle">
-            <img :src="icon_user_bind">
-            <span class="bindText">输入电话号码绑定用户</span>
-          </div>
-          <van-field
-            v-model="mobilePhone"
-            clearable
-            label="手机号码"
-            maxlength="30"
-            label-width="65px"
-            placeholder="请输入手机号"
-          />
+        <div class="bindTitle">
+          <img :src="icon_user_bind">
+          <span class="bindText">输入电话号码绑定用户</span>
+        </div>
+        <van-field
+          v-model="mobilePhone"
+          clearable
+          label="手机号码"
+          maxlength="30"
+          label-width="65px"
+          placeholder="请输入手机号"
+        />
         <div class="verifyCodeBox">
           <van-field
             v-model="quickPayVerifyCode"
@@ -28,22 +28,23 @@
                       @click="onGetVerifyCodeBtnClick">{{verifyBtnText}}
           </van-button>
         </div>
-          <div style="width:95%;margin: 10px;display: flex; flex-direction:column;text-align: center;align-items: center">
-            <van-button size="large" type="danger" round @click="onBCardBindBtnClick">绑定</van-button>
-            <div style="margin: 10px 2px;display: flex;justify-items: center;text-align: center;">
-              <p style="font-size: 10pt">
-                <span style=" color: #1989fa;">{{getBindInfo()}}</span>
-              </p>
-            </div>
+        <div style="width:95%;margin: 10px;display: flex; flex-direction:column;text-align: center;align-items: center">
+          <van-button size="large" type="danger" round @click="onBCardBindBtnClick">绑定</van-button>
+          <div style="margin: 10px 2px">
+            <p style="font-size: 10pt">
+              <span style=" color: #1989fa;">{{getBindInfo()}}</span>
+            </p>
           </div>
-
         </div>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import Header from '@/common/_header.vue'
+  import Util from '@/util/common'
 
   export default {
     components: {
@@ -68,7 +69,30 @@
       }
     },
     methods: {
-      getBindInfo(){
+      countDown() {
+        this.verifyCodeCount--;
+        if (this.verifyCodeCount <= 0) {
+          clearInterval(this.verifyCodeTimer)
+          this.verifyBtnTextClicked = false;
+          this.verifyCodeTimer = 0;
+          this.verifyCodeCount = 0
+          this.verifyBtnText = '获取验证码'
+          this.isVerifyCodeBtnDisabled = false;
+        } else {
+          this.verifyBtnText = this.verifyCodeCount + ' s'
+        }
+      },
+
+      onGetVerifyCodeBtnClick() {
+        this.$log("onGetVerifyCodeBtnClick Enter")
+        if (!this.isVerifyCodeBtnDisabled) {
+          this.isVerifyCodeBtnDisabled = true;
+          this.verifyCodeCount = 60
+          this.verifyCodeTimer = setInterval(this.countDown, 1000);
+          this.verifyBtnTextClicked = true;
+        }
+      },
+      getBindInfo() {
         switch (this.$api.APP_ID) {
           case '11': {
             return "绑定无锡市民卡App用户"
@@ -84,6 +108,7 @@
 
 <style lang="less" scoped>
   @import "../assets/fz.less";
+
   .login {
     width: 100%;
     height: 100%;
@@ -132,16 +157,20 @@
         }
 
         .verifyCodeBox {
+          width: 100%;
           display: flex;
-          padding: 10px;
-          width:  100%;
+          flex-direction: row;
+          margin-top: 2px;
           .van-field {
-            width: 60%;
+            margin-right: 10px;
           }
 
           .van-button {
-            width: 40%;
-            margin-left: 10px;
+            float: right;
+            height: 100%;
+            min-width: 8em;
+            padding: 3px 0px;
+            margin: 2px;
           }
         }
       }
