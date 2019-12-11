3338df2bbdf563e475e6cdc74b3e7e5cf94f5946
Jon.Zhou <jon-zhou@qq.com>
2019-12-11 14:40:27 +0800

    change version

--- src/views/User.vue
+++ src/views/User.vue
@@ -82,7 +82,7 @@
         <van-cell title="客服电话" :value="this.$api.SERVR_PHONE_NUM">
           <img slot="icon" :src="servicePhoneIcon"/>
         </van-cell>
-        <van-cell title="关于我们" value="版本号 V1.0">
+        <van-cell title="关于我们" value="版本号 V3.0">
           <img slot="icon" :src="aboutIcon"/>
         </van-cell>
       </div>
