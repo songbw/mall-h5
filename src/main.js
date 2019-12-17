dec7a1200eff107ed0bac9e69b2317192dd19018
Jon.Zhou <jon-zhou@qq.com>
2019-12-17 18:04:10 +0800

    remove log

--- src/main.js
+++ src/main.js
@@ -80,7 +80,7 @@ if (process.env.NODE_ENV === 'production') {
       second = second < 10 ? ('0' + second) : second;
       return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
     },
-    dev: true,
+    dev: false,
     levels: ["log", "warn", "debug", "error", "dir"],
     forceLevels: []
   })
