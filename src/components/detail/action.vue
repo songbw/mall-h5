93c6506c0b9793d77951153a41723c868a7d2a41
Jon.Zhou <jon-zhou@qq.com>
2020-02-04 18:19:14 +0800

    change UI”

--- src/components/detail/action.vue
+++ src/components/detail/action.vue
@@ -20,10 +20,12 @@
       <van-sku
         v-model="showBase"
         :sku="sku"
-        goods="测试商品"
-        goods-id="1"
+        :goods="goods"
+        :goods-id="datas.mpu"
         hide-stock= true
         disable-stepper-input=true
+        @buy-clicked="onBuyClicked"
+        @add-cart="onAddCartClicked"
       />
     </div>
 
@@ -50,7 +52,8 @@
         chCouponIcon: require('@/assets/icons/ico_changecoupon.png'),
         cartNumber: 0,
         showBase: false,
-        sku:null
+        sku:null,
+        goods: null,
       }
     },
 
@@ -72,11 +75,62 @@
         this.$log("datas changed @@@@@@@@@@@@@@@@@@@")
         this.$log(newValue)
         if(newValue != null) {
+          this.goods = {
+            // 商品标题
+            title: '',
+            // 默认商品 sku 缩略图
+            picture: this.datas.image
+          }
           if(this.datas.skuList != undefined && this.datas.skuList.length > 0) {
+            let tree = [];
+            let list = [];
+            this.datas.skuList.forEach(sku => {
+              sku.propertyList.forEach(property => {
+                let foundKey = -1;
+                for(let i = 0; i < tree.length; i++) {
+                  if(tree[i].k == property.name) {
+                    foundKey = 1;
+                    let foundVal = -1;
+                    for(let j = 0; j <tree[i].v.length; j++) {
+                      if(tree[i].v[j].name == property.val) {
+                        foundVal = 1;
+                        break;
+                      }
+                    }
+                    if(foundVal != -1) {
+                      tree[i].v.push({
+                        name: property.val,
+                        imgUrl:sku.goodsLogo
+                      })
+                    }
+                    break;
+                  }
+                }
+                if(foundKey == -1) {
+                  tree.push({
+                    k: property.name,
+                    v:[
+                      {
+                        name: property.val,
+                        imgUrl:sku.goodsLogo
+                      }
+                    ]
+                  })
+                }
+              })
+              list.push({
+                id:sku.skuId,
+                price: parseInt((this.datas.price * 100).toFixed(0)),
+                stock_num: 100
+              })
+            })
+            this.$log("!!!!!!!!!!!!!!!!!!!!!!!")
+            this.$log(tree)
+            this.$log(list)
             this.sku =  {
               // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
               // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
-              tree: [
+/*              tree: [
                 {
                   k: '颜色', // skuKeyName：规格类目名称
                   v: [
@@ -93,23 +147,20 @@
                   ],
                   k_s: 's1' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
                 }
-              ],
+              ],*/
+              tree: tree,
+              list: list,
               // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
-              list: [
-                {
-                  id: 2259, // skuId，下单时后端需要
-                  price: 100, // 价格（单位分）
-                  s1: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
-                  s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
-                  s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-                  stock_num: 110 // 当前 sku 组合对应的库存
-                }
-              ],
-              price: '1.00', // 默认价格（单位元）
-              stock_num: 227, // 商品总库存
-              collection_id: 2261, // 无规格商品 skuId 取 collection_id，否则取所选 sku 组合对应的 id
-              none_sku: false, // 是否无规格商品
-              hide_stock: false // 是否隐藏剩余库存
+              // list: [
+              //   {
+              //     id: 2259, // skuId，下单时后端需要
+              //     price: 100, // 价格（单位分）
+              //     s1: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
+              //     s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
+              //     s3: '0', // 最多包含3个规格值，为0表示不存在该规格
+              //     stock_num: 110 // 当前 sku 组合对应的库存
+              //   }
+              // ],
             }
           }
         }
@@ -127,6 +178,13 @@
     },
 
     methods: {
+      onBuyClicked(skuData) {
+        this.$log("onBuyClicked Enter")
+      },
+      onAddCartClicked(skuData) {
+        this.$log("onAddCartClicked Enter")
+        this.$log(skuData)
+      },
       getClientName() {
         switch (this.$api.APP_ID) {
           case "09":
