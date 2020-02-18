94287c712e541b689f615f1a8c2e6a632cb71550
Jon.Zhou <jon-zhou@qq.com>
2020-02-18 17:54:00 +0800

    add billoflading page

---
+++ src/components/user/billOfLadingCard.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <section class="main">
+    <span>提货券</span>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+
+    created() {
+
+    },
+
+    methods: {
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import "../../assets/fz.less";
+  @import "../../assets/index/style.css";
+
+  .main {
+    width: 100%;
+    height: 100%;
+    top: 0px;
+
+
+  }
+
+</style>
