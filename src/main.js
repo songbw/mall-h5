b94ec0d6b86e796d0533a793b0db28382d08c6cc
Jon.Zhou <jon-zhou@qq.com>
2020-03-07 18:29:17 +0800

    login ok

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import LyTab from 'ly-tab'
 //import ba from 'vue-ba'
 import VueWechatTitle from 'vue-wechat-title'
 import VueQRCodeComponent from 'vue-qrcode-component'
-//import VConsole from 'vconsole'
+import VConsole from 'vconsole'
 import md5 from 'js-md5';
 import {
   Button,
@@ -55,9 +55,9 @@ import {
   Picker
 } from 'vant';
 
-/* if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   new VConsole()
-} */
+}
 
 Vue.component('qr-code', VueQRCodeComponent)
 Vue.use(VueWechatTitle)
@@ -81,7 +81,7 @@ if (process.env.NODE_ENV === 'production') {
       second = second < 10 ? ('0' + second) : second;
       return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
     },
-    dev: false,
+    dev: true,
     levels: ["log", "warn", "debug", "error", "dir"],
     forceLevels: []
   })
